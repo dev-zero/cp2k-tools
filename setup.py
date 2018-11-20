99c780a69d89954718162e011ecaf8e1fdc895ad
Tiziano Müller <tiziano.mueller@chem.uzh.ch>
2018-11-20 09:54:53 +0100

    Switch to Apache-2.0 license

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX',
         'Programming Language :: Python',
     ],
